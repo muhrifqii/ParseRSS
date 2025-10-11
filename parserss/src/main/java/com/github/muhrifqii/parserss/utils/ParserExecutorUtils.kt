a8a1599e2fa6de831c77dfe15cf677ee989e700e
Muhammad Rifqi Fatchurrahman <muhrifqii@users.noreply.github.com>
2025-10-11 14:55:47 +0700

    [KMP] Using abstraction layer on Xml Pull Parser  (#17)
    * add abstraction layer on xml pull parser
    
    * prepare bump 0.9.0
    
    * fix code quality check issue
    
    * update readme
    
    * fix detekt findings

--- parserss/src/main/java/com/github/muhrifqii/parserss/utils/ParserExecutorUtils.kt
+++ parserss/src/main/java/com/github/muhrifqii/parserss/utils/ParserExecutorUtils.kt
@@ -5,31 +5,37 @@ import com.github.muhrifqii.parserss.ParsingMode
 import com.github.muhrifqii.parserss.RSSFeed
 import com.github.muhrifqii.parserss.RSSVersion
 import com.github.muhrifqii.parserss.RootDocument
+import com.github.muhrifqii.parserss.internal.element.ParseRSSElement
 
 object ParserExecutorUtils {
 
-    private fun deduceDefaultNSParsingMode(root: RSSFeed, element: ParseRSSElement) = when (element.name) {
-        RSSVersion.RSS_V2.elementName -> RootDocument.RSS
-        ParseRSSKeyword.CHANNEL -> ParsingMode.Channel(root, false)
-        ParseRSSKeyword.ITEM -> ParsingMode.Item {
-            root.items.add(it)
-        }
-        ParseRSSKeyword.IMAGE -> ParsingMode.Image {
-            root.image = it
-        }
-        RSSVersion.RSS_ATOM.elementName -> RootDocument.Atom + ParsingMode.Channel(root, false)
-        ParseRSSKeyword.ENTRY -> ParsingMode.Item {
-            root.items.add(it)
-        }
-        ParseRSSKeyword.AUTHOR -> ParsingMode.Author()
-        else -> ParsingMode.Read()
-    }
+    private fun deduceDefaultNSParsingMode(root: RSSFeed, element: ParseRSSElement) =
+        when (element.name) {
+            RSSVersion.RSS_V2.elementName -> RootDocument.RSS
+            ParseRSSKeyword.CHANNEL -> ParsingMode.Channel(root, false)
+            ParseRSSKeyword.ITEM -> ParsingMode.Item {
+                root.items.add(it)
+            }
 
-    private fun deduceRdfNSParsingMode(root: RSSFeed, element: ParseRSSElement) = when (element.name) {
-        RSSVersion.RSS_V1.elementName -> RootDocument.RDF
-        ParseRSSKeyword.CHANNEL -> ParsingMode.Channel(root, true)
-        else -> ParsingMode.Read()
-    }
+            ParseRSSKeyword.IMAGE -> ParsingMode.Image {
+                root.image = it
+            }
+
+            RSSVersion.RSS_ATOM.elementName -> RootDocument.Atom + ParsingMode.Channel(root, false)
+            ParseRSSKeyword.ENTRY -> ParsingMode.Item {
+                root.items.add(it)
+            }
+
+            ParseRSSKeyword.AUTHOR -> ParsingMode.Author()
+            else -> ParsingMode.Read()
+        }
+
+    private fun deduceRdfNSParsingMode(root: RSSFeed, element: ParseRSSElement) =
+        when (element.name) {
+            RSSVersion.RSS_V1.elementName -> RootDocument.RDF
+            ParseRSSKeyword.CHANNEL -> ParsingMode.Channel(root, true)
+            else -> ParsingMode.Read()
+        }
 
     private fun deduceMediaNSParsingMode(element: ParseRSSElement) = when (element.name) {
         ParseRSSKeyword.GROUP -> ParsingMode.MediaNS.Group()
